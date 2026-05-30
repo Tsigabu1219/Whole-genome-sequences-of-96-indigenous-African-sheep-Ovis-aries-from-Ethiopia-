98fe8470846a81a6f0530e478d9f3a36c3835fb8
Tsigabu Gbreselassie <tsigabu.24@gmail.com>
2026-05-30 14:45:21 +0300

    update WGS pipeline script

--- WGS_data_Analysis/WGS_data_analysis.sh
+++ WGS_data_Analysis/WGS_data_analysis.sh
@@ -24,7 +24,8 @@ t="12"
 TH="4"
 dbSNP="/home/tsigabu/V2_dbSNP/indexed/GCA_016772045.1_current_ids.vcf.gz"
 ^^^^^^^^^^^^^^^^^^^^^^^^^
-RGSM=(ABETF0000003 
+RGSM=(
+ABETF0000003 
 ABETF0000005 
 ABETF0000007 
 ABETF0000008 
@@ -194,188 +195,29 @@ java -Xmx80G -jar ${GATK}/gatk-package-4.3.0.0-local.jar BaseRecalibratorSpark \
                 -G AS_StandardAnnotation
 11.#Combine GVCFs 
 #!/bin/bash/
-gatk CombineGVCFs \
--R "/home/tgebreselassie/REF/ARS-UI_Ramb_v2.0_genomic.fa"
---dbsnp “/home/tgebreselassie/V2_dbSNP/indexed/GCA_016772045.1_current_ids.vcf.gz” 
---variant ABETF0000003.g.vcf.gz 
---variant ABETF0000005.g.vcf.gz 
---variant ABETF0000007.g.vcf.gz 
---variant ABETF0000008.g.vcf.gz 
---variant ABETF0000009.g.vcf.gz 
---variant ABETF0000012.g.vcf.gz 
---variant ABETF0000013.g.vcf.gz 
---variant ABETF0000014.g.vcf.gz 
---variant ABETF0000015.g.vcf.gz 
---variant ABETF0000016.g.vcf.gz 
---variant ABETF0000020.g.vcf.gz 
---variant ABETF0000021.g.vcf.gz 
---variant ABETF0000023.g.vcf.gz 
---variant ABETF0000026.g.vcf.gz 
---variant ABETF0000033.g.vcf.gz 
---variant ABETF0000034.g.vcf.gz 
---variant ABETF0000035.g.vcf.gz 
---variant ABETF0000036.g.vcf.gz 
---variant ABETM0000006.g.vcf.gz 
---variant ABETM0000037.g.vcf.gz 
---variant BGETF0000002.g.vcf.gz 
---variant BGETF0000003.g.vcf.gz 
---variant BGETF0000004.g.vcf.gz 
---variant BGETM0000005.g.vcf.gz 
---variant BGETF0000006.g.vcf.gz 
---variant BGETF0000007.g.vcf.gz 
---variant BGETF0000008.g.vcf.gz 
---variant BGETM0000010.g.vcf.gz 
---variant BGETM0000012.g.vcf.gz 
---variant BGETF0000013.g.vcf.gz 
---variant BGETF0000014.g.vcf.gz 
---variant BGETF0000015.g.vcf.gz 
---variant BGETF0000016.g.vcf.gz 
---variant BGETF0000018.g.vcf.gz 
---variant BGETF0000019.g.vcf.gz 
---variant BGETM0000020.g.vcf.gz 
---variant BGETM0000021.g.vcf.gz 
---variant BGETF0000022.g.vcf.gz 
---variant BGETM0000023.g.vcf.gz 
---variant BGETM0000024.g.vcf.gz 
---variant BGETM0000025.g.vcf.gz 
---variant BGETF0000026.g.vcf.gz 
---variant BGETF0000027.g.vcf.gz 
---variant ELETF0000001.g.vcf.gz 
---variant ELETF0000002.g.vcf.gz 
---variant ELETF0000003.g.vcf.gz 
---variant ELETF0000004.g.vcf.gz 
---variant ELETF0000005.g.vcf.gz 
---variant ELETF0000006.g.vcf.gz 
---variant ELETF0000007.g.vcf.gz 
---variant ELETF0000008.g.vcf.gz 
---variant ELETM0000009.g.vcf.gz 
---variant ELETF0000011.g.vcf.gz 
---variant ELETF0000012.g.vcf.gz 
---variant ELETF0000013.g.vcf.gz 
---variant ELETF0000014.g.vcf.gz 
---variant ELETF0000016.g.vcf.gz 
---variant ELETF0000017.g.vcf.gz 
---variant ELETF0000018.g.vcf.gz 
---variant ELETF0000019.g.vcf.gz 
---variant ELETM0000020.g.vcf.gz 
---variant ELETM0000021.g.vcf.gz 
---variant ELETM0000022.g.vcf.gz 
---variant MEETM0000018.g.vcf.gz 
---variant MEETF0000022.g.vcf.gz 
---variant MEETF0000023.g.vcf.gz 
---variant MEETF0000024.g.vcf.gz 
---variant MEETF0000025.g.vcf.gz 
---variant MEETF0000026.g.vcf.gz 
---variant MEETF0000027.g.vcf.gz 
---variant MEETF0000031.g.vcf.gz 
---variant MEETF0000033.g.vcf.gz 
---variant MEETF0000035.g.vcf.gz 
---variant MEETF0000036.g.vcf.gz 
---variant MEETF0000037.g.vcf.gz 
---variant MEETF0000041.g.vcf.gz 
---variant MEETF0000042.g.vcf.gz 
---variant MEETF0000045.g.vcf.gz 
---variant NUETF0000001.g.vcf.gz 
---variant NUETF0000011.g.vcf.gz 
---variant NUETM0000002.g.vcf.gz 
---variant NUETM0000046.g.vcf.gz 
---variant NUETM0000048.g.vcf.gz 
---variant NUETF0000049.g.vcf.gz 
---variant NUETF0000050.g.vcf.gz 
---variant NUETF0000051.g.vcf.gz 
---variant NUETF0000052.g.vcf.gz 
---variant NUETF0000053.g.vcf.gz 
---variant NUETF0000056.g.vcf.gz 
---variant NUETF0000057.g.vcf.gz 
---variant NUETF0000059.g.vcf.gz 
---variant NUETF0000060.g.vcf.gz 
---variant NUETF0000062.g.vcf.gz 
---variant NUETF0000061.g.vcf.gz 
---variant NUETF0000066.g.vcf.gz 
---variant NUETM0000070.g.vcf.gz 
---output 96_samples.g.vcf.gz
+gatk CombineGVCFs -R "/home/tgebreselassie/REF/ARS-UI_Ramb_v2.0_genomic.fa" --dbsnp "/home/tgebreselassie/V2_dbSNP/indexed/GCA_016772045.1_current_ids.vcf.gz" --variant ABETF0000003.g.vcf.gz --variant ABETF0000005.g.vcf.gz --variant ABETF0000007.g.vcf.gz --variant ABETF0000008.g.vcf.gz --variant ABETF0000009.g.vcf.gz --variant ABETF0000012.g.vcf.gz --variant ABETF0000013.g.vcf.gz --variant ABETF0000014.g.vcf.gz --variant ABETF0000015.g.vcf.gz -variant ABETF0000016.g.vcf.gz --variant ABETF0000020.g.vcf.gz --variant ABETF0000021.g.vcf.gz --variant ABETF0000023.g.vcf.gz --variant ABETF0000026.g.vcf.gz --variant ABETF0000033.g.vcf.gz --variant ABETF0000034.g.vcf.gz --variant ABETF0000035.g.vcf.gz --variant ABETF0000036.g.vcf.gz --variant ABETM0000006.g.vcf.gz --variant ABETM0000037.g.vcf.gz --variant BGETF0000002.g.vcf.gz --variant BGETF0000003.g.vcf.gz --variant BGETF0000004.g.vcf.gz --variant BGETM0000005.g.vcf.gz --variant BGETF0000006.g.vcf.gz --variant BGETF0000007.g.vcf.gz --variant BGETF0000008.g.vcf.gz --variant BGETM0000010.g.vcf.gz --variant BGETM0000012.g.vcf.gz --variant BGETF0000013.g.vcf.gz --variant BGETF0000014.g.vcf.gz --variant BGETF0000015.g.vcf.gz --variant BGETF0000016.g.vcf.gz --variant BGETF0000018.g.vcf.gz --variant BGETF0000019.g.vcf.gz --variant BGETM0000020.g.vcf.gz --variant BGETM0000021.g.vcf.gz --variant BGETF0000022.g.vcf.gz --variant BGETM0000023.g.vcf.gz --variant BGETM0000024.g.vcf.gz --variant BGETM0000025.g.vcf.gz --variant BGETF0000026.g.vcf.gz --variant BGETF0000027.g.vcf.gz --variant ELETF0000001.g.vcf.gz --variant ELETF0000002.g.vcf.gz --variant ELETF0000003.g.vcf.gz --variant ELETF0000004.g.vcf.gz --variant ELETF0000005.g.vcf.gz --variant ELETF0000006.g.vcf.gz --variant ELETF0000007.g.vcf.gz --variant ELETF0000008.g.vcf.gz --variant ELETM0000009.g.vcf.gz --variant ELETF0000011.g.vcf.gz --variant ELETF0000012.g.vcf.gz --variant ELETF0000013.g.vcf.gz --variant ELETF0000014.g.vcf.gz --variant ELETF0000016.g.vcf.gz --variant ELETF0000017.g.vcf.gz --variant ELETF0000018.g.vcf.gz --variant ELETF0000019.g.vcf.gz --variant ELETM0000020.g.vcf.gz --variant ELETM0000021.g.vcf.gz --variant ELETM0000022.g.vcf.gz --variant MEETM0000018.g.vcf.gz --variant MEETF0000022.g.vcf.gz --variant MEETF0000023.g.vcf.gz --variant MEETF0000024.g.vcf.gz --variant MEETF0000025.g.vcf.gz --variant MEETF0000026.g.vcf.gz --variant MEETF0000027.g.vcf.gz --variant MEETF0000031.g.vcf.gz --variant MEETF0000033.g.vcf.gz --variant MEETF0000035.g.vcf.gz --variant MEETF0000036.g.vcf.gz --variant MEETF0000037.g.vcf.gz --variant MEETF0000041.g.vcf.gz --variant MEETF0000042.g.vcf.gz --variant MEETF0000045.g.vcf.gz --variant NUETF0000001.g.vcf.gz --variant NUETF0000011.g.vcf.gz --variant NUETM0000002.g.vcf.gz --variant NUETM0000046.g.vcf.gz --variant NUETM0000048.g.vcf.gz --variant NUETF0000049.g.vcf.gz --variant NUETF0000050.g.vcf.gz --variant NUETF0000051.g.vcf.gz --variant NUETF0000052.g.vcf.gz --variant NUETF0000053.g.vcf.gz --variant NUETF0000056.g.vcf.gz --variant NUETF0000057.g.vcf.gz --variant NUETF0000059.g.vcf.gz --variant NUETF0000060.g.vcf.gz --variant NUETF0000062.g.vcf.gz --variant NUETF0000061.g.vcf.gz --variant NUETF0000066.g.vcf.gz --variant NUETM0000070.g.vcf.gz --output 96_samples.g.vcf.gz
 
 12.#Joint genotyping
 #!bash
 module load gatk/4.3.0.0 
-gatk GenotypeGVCFs \
--R  “/home/tgebreselassie/REF/ARS-UI_Ramb_v2.0_genomic.fa “
---dbsnp /home/tgebreselassie/V2_dbSNP/indexed/GCA_016772045.1_current_ids.vcf.gz  
--V /home/tgebreselassie/output1/ABERGELLE/GVCFs/Final.Final2.119.g.vcf.gz  
--O /home/tgebreselassie/output1/ABERGELLE/VCF/96_samples.vcf.gz
+gatk GenotypeGVCFs -R  "/home/tgebreselassie/REF/ARS-UI_Ramb_v2.0_genomic.fa" --dbsnp /home/tgebreselassie/V2_dbSNP/indexed/GCA_016772045.1_current_ids.vcf.gz -V /home/tgebreselassie/output1/ABERGELLE/GVCFs/Final.Final2.119.g.vcf.gz -O /home/tgebreselassie/output1/ABERGELLE/VCF/96_samples.vcf.gz
 13.#VQSR (Variant Quality Score Recalibration)
 VQSR for SNP
-Gatk VariantRecalibrator \
--R "/home/tgebreselassie/REF/ARS-UI_Ramb_v2.0_genomic.fa"  \
--V 96_samples.vcf.gz \
--resource:dbsnp,known=false,training=true,truth=true,prior=15.0${knownsites} \
--resource:dbSNP,known=true,training=false,truth=false,prior=2.0 ${dbSNP}  \
--mode SNP \
--tranche 100.0 \
--tranche 99.9 \
--tranche 99.0 \
--an QD \
--an MQRankSum \
--an ReadPosRankSum \
- -an FS \
--an MQ \
--an SOR \
--an DP \
--tranche 90.0 \
--O 96_samples.SNP.recal \
---tranches-file Samples.SNP.tranches \
---rscript-file Samples_recal.plot.R\
+Gatk VariantRecalibrator -R "/home/tgebreselassie/REF/ARS-UI_Ramb_v2.0_genomic.fa" -V 96_samples.vcf.gz -resource:ensembl,known=false,training=true,truth=true,prior=15 "/home/tgebreselassie/V2_dbSNP/indexed/GCA_016772045.1_current_ids.vcf.gz" -resource:dbsnp,known=true,training=false,truth=false,prior=7 "/home/tgebreselassie/119_Omega/ovis_aries_rambouillet.vcf.gz" -mode SNP -tranche 100.0 -tranche 99.9 -tranche 99.0 -an QD -an MQRankSum -an ReadPosRankSum -an FS -an MQ -an SOR -an DP -tranche 90.0 -O 96_samples.SNP.recal --tranches-file Samples.SNP.tranches --rscript-file Samples_recal.plot.R
 14.#VQSR
  Apply VQSR for SNP
- gatk ApplyVQSR \ 
--R "/home/tgebreselassie/REF/ARS-UI_Ramb_v2.0_genomic.fa"  \
--V 96_samples.SNP.recal \
---truth-sensitivity-filter-level 99.0 \
--mode SNP \
---tranches-file Samples_recal.SNP.tranches \
---recal-file Samples_recal.SNP.recal  \
--O 96_samples.vcf.gz\
+ gatk ApplyVQSR -R "/home/tgebreselassie/REF/ARS-UI_Ramb_v2.0_genomic.fa" -V 96_samples.SNP.recal --truth-sensitivity-filter-level 99.0 -mode SNP --tranches-file Samples_recal.SNP.tranches --recal-file Samples_recal.SNP.recal -O 96_samples.vcf.gz
 #VQSR for INDELs
 gatk VariantRecalibrator 
--R "/home/REF/Reference.fa" \
--V 96_samples.vcf.gz \
---trust-all-polymorphic -tranche 100.0 \
--tranche 99.95 -tranche 99.9 \
--tranche 99.8 -tranche 99.6 \
--tranche 99.5 -tranche 99.4 \
- -tranche 99.3 \
--tranche 99.0 \
--tranche 98.0 \
--tranche 97.0 -tranche 90.0 \
--an QD -an MQRankSum \
--an ReadPosRankSum \
--an FS  \
--an MQ \
--an SOR -an DP \
---max-gaussians 4 \
--mode INDEL \
--resource:ensembl,known=false,training=true,truth=true,prior=15 "/home/tgebreselassie/V2_dbSNP/indexed/GCA_016772045.1_current_ids.vcf.gz" \
--resource:dbsnp,known=true,training=false,truth=false,prior=7 "/home/tgebreselassie/119_Omega/ovis_aries_rambouillet.vcf.gz" \
--O cohort_INDELs.recal 
---tranches-file cohort_INDELs.tranches 
+-R "/home/REF/Reference.fa" -V 96_samples.vcf.gz --trust-all-polymorphic -tranche 100.0 -tranche 99.95 -tranche 99.9 -tranche 99.8 -tranche 99.6 -tranche 99.5 -tranche 99.4 -tranche 99.3 -tranche 99.0 tranche 98.0 -tranche 97.0 -tranche 90.0 -an QD -an MQRankSum -an ReadPosRankSum -an FS -an MQ -an SOR -an DP --max-gaussians 4 -mode INDEL -resource:ensembl,known=false,training=true,truth=true,prior=15 "/home/tgebreselassie/V2_dbSNP/indexed/GCA_016772045.1_current_ids.vcf.gz" -resource:dbsnp,known=true,training=false,truth=false,prior=7 "/home/tgebreselassie/119_Omega/ovis_aries_rambouillet.vcf.gz" -O cohort_INDELs.recal --tranches-file cohort_INDELs.tranches 
 Apply VQSR for SNP
- gatk ApplyVQSR \ 
--R "/home/tgebreselassie/REF/ARS-UI_Ramb_v2.0_genomic.fa"  \
--V cohort_INDELs.tranches \
---truth-sensitivity-filter-level 99.0 \
- -mode INDEL \
---tranches-file Samples_recal.INDEL.tranches \
---recal-file Samples_recal.INDEL.recal  \
--O 96_samples.INDELs.VQSR.vcf.gz \
+ gatk ApplyVQSR -R "/home/tgebreselassie/REF/ARS-UI_Ramb_v2.0_genomic.fa" -V cohort_INDELs.tranches --truth-sensitivity-filter-level 99.0 -mode INDEL --tranches-file Samples_recal.INDEL.tranches --recal-file Samples_recal.INDEL.recal -O 96_samples.INDELs.VQSR.vcf.gz
 
 15.#Selecting only biallelic and autosomal SNPs
 gatk SelectVariants \
- -R "/home/tgebreselassie/REF/ARS-UI_Ramb_v2.0_genomic.fa"  \
- -V Sample.SNP.VQSR.vcf.gz --select-type-to-include SNP -O 96_samples.biallelic.vcf.gz --restrict-alleles-to BIALLELIC
+ -R "/home/tgebreselassie/REF/ARS-UI_Ramb_v2.0_genomic.fa" -V Sample.SNP.VQSR.vcf.gz --select-type-to-include SNP -O 96_samples.biallelic.vcf.gz --restrict-alleles-to BIALLELIC
 16.#Selecting autosomal SNPs
-gatk SelectVariants  
--R "/home/tgebreselassie/REF/ARS-UI_Ramb_v2.0_genomic.fa"  \
--V 96_samples.biallelic.vcf.gz --intervals 1 --intervals 2 --intervals 3 --intervals 4 --intervals 5 --intervals 6 --intervals 7 --intervals 8 --intervals 9 --intervals 10 --intervals 11 --intervals 12 --intervals 13 --intervals 14 --intervals 15 --intervals 16 --intervals 17 --intervals 18 --intervals 19 --intervals 20 --intervals 21 --intervals 22 --intervals 23 --intervals 24 --intervals 25 --intervals 26 -O 96_samples.vcf.gz
+gatk SelectVariants -R "/home/tgebreselassie/REF/ARS-UI_Ramb_v2.0_genomic.fa" -V 96_samples.biallelic.vcf.gz --intervals 1 --intervals 2 --intervals 3 --intervals 4 --intervals 5 --intervals 6 --intervals 7 --intervals 8 --intervals 9 --intervals 10 --intervals 11 --intervals 12 --intervals 13 --intervals 14 --intervals 15 --intervals 16 --intervals 17 --intervals 18 --intervals 19 --intervals 20 --intervals 21 --intervals 22 --intervals 23 --intervals 24 --intervals 25 --intervals 26 -O 96_samples.vcf.gz
 
 17.# BAM file summary statistics plots 
 # Load libraries
